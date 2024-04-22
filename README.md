b4f68b1c951106496b4d12242ae880a6ab597376
Nguyen Anh Tuyen <tuyen-nguyen@cybozu.vn>
2024-04-22 17:22:31 +0700

    Move Swagger viewer

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# natuyen.github.io
+# swagger-ui
+### You can see the specification of Garoon REST API by accessing such as following URLs.
+
+- Latest version  
+https://github.dev.cybozu.co.jp/pages/garoon/swagger/spec/  
+Initial view is the main branch of garoon/garoon.  
+
+
+- Specific version  
+https://github.dev.cybozu.co.jp/pages/garoon/swagger/spec/?branch=F18.8  
+set target version in "branch" parameter.  
+
+
+# swagger-editor
+### You can edit spec.yml.
+https://github.dev.cybozu.co.jp/pages/garoon/swagger/editor/
---
+++ editor/src/plugins/editor/components/editor-container.jsx
@@ -0,0 +1,75 @@
+import React from "react"
+import PropTypes from "prop-types"
+import debounce from "lodash/debounce"
+
+
+const DEBOUNCE_TIME = 800 // 0.5 imperial seconds™
+
+export default class EditorContainer extends React.Component {
+
+  constructor(props, context) {
+    super(props, context)
+    this.onChange = debounce(this._onChange.bind(this), DEBOUNCE_TIME)
+  }
+
+  _onChange(value) {
+    if(typeof this.props.onChange === "function") {
+      this.props.onChange(value)
+    }
+    this.props.specActions.updateSpec(value)
+  }
+
+  render() {
+    let { specSelectors, getComponent, errSelectors, fn, editorSelectors, configsSelectors } = this.props
+
+    let Editor = getComponent("Editor")
+
+    let wrapperClasses = ["editor-wrapper"]
+    const readOnly = !!configsSelectors.get("readOnly")
+
+    if(readOnly) {
+      wrapperClasses.push("read-only")
+    }
+
+    let propsForEditor = this.props
+
+    const editorOptions = {
+      enableLiveAutocompletion: configsSelectors.get("editorLiveAutocomplete"),
+      readOnly: readOnly,
+      highlightActiveLine: !readOnly,
+      highlightGutterLine: !readOnly,
+    }
+
+    return (
+      <div id='editor-wrapper' className={wrapperClasses.join(" ")}>
+        { readOnly ? <h2 className="editor-readonly-watermark">Read Only</h2> : null }
+        <Editor
+          {...propsForEditor}
+          value={specSelectors.specStr()}
+          editorOptions={editorOptions}
+          specObject={specSelectors.specJson().toJS()}
+          errors={errSelectors.allErrors()}
+          onChange={this.onChange}
+          goToLine={editorSelectors.gotoLine()}
+          AST={fn.AST}
+        />
+    </div>
+    )
+  }
+
+}
+
+EditorContainer.defaultProps = {
+  onChange: Function.prototype
+}
+
+EditorContainer.propTypes = {
+  specActions: PropTypes.object.isRequired,
+  configsSelectors: PropTypes.object.isRequired,
+  onChange: PropTypes.func,
+  fn: PropTypes.object,
+  specSelectors: PropTypes.object.isRequired,
+  errSelectors: PropTypes.object.isRequired,
+  editorSelectors: PropTypes.object.isRequired,
+  getComponent: PropTypes.func.isRequired,
+}
